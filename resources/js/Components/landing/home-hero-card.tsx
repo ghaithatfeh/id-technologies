586a9e54d450434838730e22f6531b6c3e3caed8
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-10-13 16:34:44 +0300

    Refactor `HomeHeroCard` for improved readability and maintainability: replaced inline background URL with style object.

--- resources/js/Components/landing/home-hero-card.tsx
+++ resources/js/Components/landing/home-hero-card.tsx
@@ -13,10 +13,13 @@ const HomeHeroCard = ({
     return (
         <div className="group border-b-landing-primary relative h-full w-full overflow-visible rounded-t-lg border-b-8 bg-white p-6 shadow-md">
             <div className="absolute inset-0 z-0">
-                <div className="absolute inset-0 rounded-t-lg bg-white bg-[length:125%_130%] bg-center bg-no-repeat"></div>
+                <div className="absolute inset-0 rounded-t-lg bg-white bg-[length:125%_130%] bg-center bg-no-repeat" />
                 <div
-                    className={`absolute inset-0 rounded-t-lg bg-[url('${background}')] bg-[length:125%_130%] bg-center bg-no-repeat opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100`}
-                ></div>
+                    className={`absolute inset-0 rounded-t-lg bg-[length:125%_130%] bg-center bg-no-repeat opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100`}
+                    style={{
+                        backgroundImage: `url('${background}')`,
+                    }}
+                />
             </div>
 
             <p className="relative z-10 text-center text-lg font-bold text-wrap">
