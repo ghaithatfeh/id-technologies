d1dd8173d56aa57757c56f5d372149a2e39c4ed8
ghaith <>
2025-10-17 01:31:23 +0300

    fix migration

--- database/migrations/2025_10_13_161135_create_products_table.php
+++ database/migrations/2025_10_13_161135_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration {
             $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete();
             $table->json('image');
             $table->json('pdf');
-            $table->string('support_link')->nullable();
             $table->timestamps();
         });
     }
