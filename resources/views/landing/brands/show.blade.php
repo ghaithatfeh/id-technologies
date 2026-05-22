3c3be902e63d8db08e64068fac75eec4f702e777
ghaith <>
2026-05-23 02:08:00 +0300

    fix space

--- resources/views/landing/brands/show.blade.php
+++ resources/views/landing/brands/show.blade.php
@@ -219,9 +219,7 @@
                                     </h1>
                                 </div>
                                 @if (isset($product->description))
-                                    <p class="h-full p-2 text-center text-sm whitespace-pre-line">
-                                        {{ $product->description }}
-                                    </p>
+                                    <p class="h-full p-2 text-center text-sm whitespace-pre-line">{{ $product->description }}</p>
                                 @endif
 
                                 <a
